2c5a0df1d96580c321ed495b03f812514dd3b6ce
Ameeshaarora <ameeshaarora122@gmail.com>
2023-07-17 22:27:09 +0530

    Initial commit

---
+++ app/src/main/java/com/ameesha/fragmentregister/fragment_container.kt
@@ -0,0 +1,11 @@
+package com.ameesha.fragmentregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fragment_container : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_container)
+    }
+}
