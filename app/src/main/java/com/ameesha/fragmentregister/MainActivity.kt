b6a4b2378d0013c2db25b4f1626ac8e2d6551092
Ameeshaarora <ameeshaarora122@gmail.com>
2023-07-19 14:39:02 +0530

    view binding
    delete some extra file

--- app/src/main/java/com/ameesha/fragmentregister/MainActivity.kt
+++ app/src/main/java/com/ameesha/fragmentregister/MainActivity.kt
@@ -2,13 +2,26 @@ package com.ameesha.fragmentregister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.ameesha.fragmentregister.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    var binding: ActivityMainBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnValidate?.setOnClickListener {
+            if (binding?.etName?.text?.toString().isNullOrEmpty()){
+                binding?.etName?.error="Enter your name"
+            }
+            else if (binding?.etCollegeName?.text?.toString().isNullOrEmpty()){
+                binding?.etCollegeName?.error="Enter your Address"
+            }
+            else
+                Toast.makeText(this,"validate",Toast.LENGTH_SHORT).show()
+        }
     }
 }
